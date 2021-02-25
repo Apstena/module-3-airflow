4dd93c0f437a91af24d40db45fdeaae54cd2d4ee
AgasgG <63339483+AgasgG@users.noreply.github.com>
2021-02-25 22:36:31 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -15,17 +15,19 @@ default_args = {
 
 dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
-t1 = BashOperator(
-    task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
-    dag=dag
-)
+rocket_list = ['falcon1', 'falcon9', 'falconheavy', 'all', ]
+for rocket in rocket_list:
+    t1 = BashOperator(
+        task_id="get_data_" + rocket,
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data",
+        dag=dag
+    )
 
-t2 = BashOperator(
-    task_id="print_data", 
-    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-    params={"rocket": "all"}, # falcon1/falcon9/falconheavy
-    dag=dag
-)
+    t2 = BashOperator(
+        task_id="print_data_" + rocket,
+        bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv",
+        params={"rocket": rocket},  # falcon1/falcon9/falconheavy
+        dag=dag
+    )
 
 t1 >> t2
