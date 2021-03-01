a02d4241015f9bc34426d2780ee99312516cac1b
Apstena <79761048+Apstena@users.noreply.github.com>
2021-03-02 00:29:42 +0700

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -19,8 +19,8 @@ rlist = ['falconheavy','falcon1','falcon9','all']
 for i in rlist:
     t1 = BashOperator(
         task_id="get_data"+i, 
-        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{{{ execution_date.year }}}} -o /var/data{}".format(" -r {{getparams.rocket}}" if i !='all' else ""), 
-        getparams={"rocket": i}, # falcon1/falcon9/falconheavy
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{{{ execution_date.year }}}} -o /var/data{}".format(" -r {{gparams.rocket}}" if i !='all' else ""), 
+        gparams={"rocket": i}, # falcon1/falcon9/falconheavy
         dag=dag
     )
 
