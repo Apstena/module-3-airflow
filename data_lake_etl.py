5e96c5debff4353808545d92bfe0888024d951e6
AgasgG <63339483+AgasgG@users.noreply.github.com>
2021-04-11 21:26:10 +0300

    add dm (testing)

--- data_lake_etl.py
+++ data_lake_etl.py
@@ -28,19 +28,34 @@ tables = {'ods_billing': ['user_id, billing_period, service, tariff, CAST(sum as
               'stg_payment', 'pay_date'],
           'ods_traffic': [
               'user_id, CAST(CAST(`timestamp` as BIGINT) as TIMESTAMP), device_id, device_ip_addr, CAST(bytes_sent as INT), CAST(bytes_received as INT)',
-              'stg_traffic', 'CAST(CAST(`timestamp` as BIGINT) as TIMESTAMP)']}
+              'stg_traffic', 'CAST(CAST(`timestamp` as BIGINT) as TIMESTAMP)'],
+          'dm_traffic' : ['user_id, MAX(bytes_received), MIN(bytes_received), AVG(bytes_received)', 'ods_traffic', 'year']}
 params = {'current_year' : 2012, 'job_suffix': randint(0, 100000)}
 
 while params['current_year'] <= 2021:
     for i in tables:
-        data_proc = DataProcHiveOperator(
-            task_id = i,
-            dag = dag,
-            query = """INSERT OVERWRITE TABLE alevanov.{3} PARTITION (year={4})
-            SELECT {0} FROM alevanov.{1} WHERE year({2}) = {4};""".format(tables[i][0], tables[i][1],
-                                                                                                tables[i][2], i, params['current_year']),
-            cluster_name = 'cluster-dataproc',
-            job_name = USERNAME + '_{0}_{1}_{2}'.format(i, params['current_year'], params['job_suffix']),
-            region = 'europe-west3'
-        )
-    params['current_year'] += 1
+        if i != 'dm_traffic':
+            data_proc = DataProcHiveOperator(
+                task_id = i,
+                dag = dag,
+                query = """INSERT OVERWRITE TABLE alevanov.{3} PARTITION (year={4})
+                SELECT {0} FROM alevanov.{1} WHERE year({2}) = {4};""".format(tables[i][0], tables[i][1],
+                                                                                                    tables[i][2], i, params['current_year']),
+                cluster_name = 'cluster-dataproc',
+                job_name = USERNAME + '_{0}_{1}_{2}'.format(i, params['current_year'], params['job_suffix']),
+                region = 'europe-west3'
+            )
+            if i == 'ods_traffic':
+                date_proc_dm = DataProcHiveOperator(
+                    task_id='dm_traffic',
+                    dag=dag,
+                    query="""INSERT OVERWRITE TABLE alevanov.{3} PARTITION (year={4})
+                            SELECT {0} FROM alevanov.{1} WHERE year({2}) = {4} GROUP BY user_id;""".format(tables['dm_traffic'][0], tables['dm_traffic'][1],
+                                                                                          tables['dm_traffic'][2], 'dm_traffic',
+                                                                                          params['current_year']),
+                    cluster_name='cluster-dataproc',
+                    job_name=USERNAME + '_{0}_{1}_{2}'.format('dm_traffic', params['current_year'], params['job_suffix']),
+                    region='europe-west3'
+                )
+                data_proc >> date_proc_dm
+        params['current_year'] += 1
